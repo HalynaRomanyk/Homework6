d26621b4faf46e306a8801c035fa3872e4e9ae87
HalynaRomanyk <96209010+HalynaRomanyk@users.noreply.github.com>
2022-01-13 09:54:43 +0200

    formatted the code

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -15,24 +15,26 @@ public class Main {
         }
         {
 //            task 2
-            ArrayList<String> fruit = new ArrayList<>(List.of("Apple", "Kivi", "Lemon", "Mango", "Banana", "Orange"));
+            ArrayList<String> fruit = new ArrayList<>(List.of("Apple", "Kiwi", "Lemon", "Mango", "Banana", "Orange"));
             System.out.println(fruit);
 
             int index = fruit.indexOf("Orange");
-            if (index != -1)
+            if (index != -1) {
                 fruit.set(index, "Grapefruit");
-            System.out.println(fruit);
-        }
-        {
+                System.out.println(fruit);
+            } else {
+                System.out.println("Value not present");
+            }
+            {
 //            task 3
-            ArrayList<String> calendar1 = new ArrayList<>(List.of("01/01/21", "15/03/18", "28/08/16", "31/12/20"));
-            ArrayList<String> calendar2 = new ArrayList<>(List.of("31/12/20", "21/04/16", "17/09/21", "06/07/19"));
-            System.out.println(calendar1);
-            System.out.println(calendar2);
+                ArrayList<String> calendar1 = new ArrayList<>(List.of("01/01/21", "15/03/18", "28/08/16", "31/12/20"));
+                ArrayList<String> calendar2 = new ArrayList<>(List.of("31/12/20", "21/04/16", "17/09/21", "06/07/19"));
+                System.out.println(calendar1);
+                System.out.println(calendar2);
 
-            calendar1.retainAll(calendar2);
-            System.out.println("Similar date: "
-                    + calendar1);
+                calendar1.retainAll(calendar2);
+                System.out.println("Similar date: " + calendar1);
+            }
         }
     }
 }
